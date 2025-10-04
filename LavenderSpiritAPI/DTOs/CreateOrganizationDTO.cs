2605f59f4dbccf0db35bdfc908315f6b66153f13
Mateusz Poszelężny <poszeleznymateusz@gmail.com>
2025-10-04 22:59:59 +0200

    Organization Begining

---
+++ LavenderSpiritAPI/DTOs/CreateOrganizationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LavenderSpiritAPI.DTOs
+{
+    public class CreateOrganizationDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ LavenderSpiritAPI/Models/Organization.cs
@@ -0,0 +1,12 @@
+﻿namespace LavenderSpiritAPI.Models
+{
+    public class Organization
+    {
+        public Guid OrganizationID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public virtual IEnumerable<LavEvent> HostedEvents { get; set; } = new List<LavEvent>();
+    }
+}
