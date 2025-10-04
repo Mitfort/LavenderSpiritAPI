b946b8b677f0799e5ed9adf1a85c66feff6d5547
Rafał Jarczyk <rafal@Rafas-MacBook-Pro.local>
2025-10-04 14:48:27 +0200

    auto commit

--- LavenderSpiritAPI/DTOs/EventDTO.cs
+++ LavenderSpiritAPI/DTOs/EventDTO.cs
@@ -2,5 +2,8 @@
 {
     public class EventDTO
     {
+        public string EventName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
     }
 }
---
+++ LavenderSpiritAPI/DTOs/OrganizationDTO.cs
@@ -0,0 +1,6 @@
+namespace LavenderSpiritAPI.DTOs;
+
+public class OrganizationDTO
+{
+    public string OrganizationName { get; set; }
+}
