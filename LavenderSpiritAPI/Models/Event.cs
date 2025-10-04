e2d2cabb65d9514eba66ef73f7d4c392cdeac2b4
Kuba <kkubajan09@gmail.com>
2025-10-04 12:47:02 +0200

    Remove Entieties dir and init Event.cs

--- LavenderSpiritAPI/Models/Event.cs
+++ LavenderSpiritAPI/Models/Event.cs
@@ -1,6 +1,26 @@
-﻿namespace LavenderSpiritAPI.Models
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LavenderSpiritAPI.Models
 {
     public class Event
     {
+        [NotNull]
+        public Guid EventID { get; set; }
+
+#nullable disable
+        [NotNull]
+        public string EventName { get; set; }
+
+        [NotNull]
+        public string Description { get; set; }
+
+        [NotNull]
+        public string PhotoPath { get; set; }
+#nullable enable
+
+        public DateTime DateTime { get; set; }
+        public DateTime CreationDate { get; set; }
+
+
     }
 }
