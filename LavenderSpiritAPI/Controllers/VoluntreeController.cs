03fa37885f92eddb04127137606d8469a8137014
Marysia <mbludova@student.agh.edu.pl>
2025-10-04 19:08:17 +0200

    Add Login

--- LavenderSpiritAPI/Controllers/VoluntreeController.cs
+++ LavenderSpiritAPI/Controllers/VoluntreeController.cs
@@ -6,18 +6,30 @@ namespace LavenderSpiritAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class VoluntreeController: ControllerBase
+    public class VoluntreeController : ControllerBase
     {
         private readonly IVolunteerService _volunteerService;
-        public VoluntreeController(IVolunteerService volunteerService) 
-        { 
+        public VoluntreeController(IVolunteerService volunteerService)
+        {
             _volunteerService = volunteerService;
         }
 
-        [HttpPost]
-        public IActionResult CreateVoluntre([FromBody]CreateVoluntreeDTO dTO)
+        [HttpPost("register")]
+        public IActionResult CreateVoluntre([FromBody] CreateVoluntreeDTO dTO)
         {
             _volunteerService.CreateVolunteer(dTO);
+
+            return Ok();
+        }
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDTO dto)
+        {
+            var user = _volunteerService.Login(dto.Email, dto.Password);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
             return Ok();
         }
     }
--- LavenderSpiritAPI/Services/IVolunteerService.cs
+++ LavenderSpiritAPI/Services/IVolunteerService.cs
@@ -1,9 +1,11 @@
 ﻿using LavenderSpiritAPI.DTOs;
+using LavenderSpiritAPI.Models;
 
 namespace LavenderSpiritAPI.Services
 {
     public interface IVolunteerService
     {
         public Guid CreateVolunteer(CreateVoluntreeDTO dTO);
+        public Voluntree? Login(LoginDTO dTO);
     }
 }
