914c2addde6eb8ce53e8739b5a88b7a78b76c047
Mateusz Poszelężny <poszeleznymateusz@gmail.com>
2025-10-04 14:41:25 +0200

    NWM

---
+++ LavenderSpiritAPI/Models/Organization.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.ComponentModel.DataAnnotations;
+
+namespace LavenderSpiritAPI.Models
+{
+    public class Organization
+    {
+        public Guid OrganizationID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public User Creator { get; set; }
+        public DateTime CreationDate { get; set; }
+        public List<User> Members { get; set; } = new List<User>();
+        public List<Event> Events { get; set; } = new List<Event>();
+    }
+}
