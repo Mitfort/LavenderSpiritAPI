00ad91df45464dc5719833765f0a474a465d9642
Marysia <mbludova@student.agh.edu.pl>
2025-10-04 23:26:22 +0200

    Update CreateOrganizationDTO.cs

--- LavenderSpiritAPI/DTOs/CreateOrganizationDTO.cs
+++ LavenderSpiritAPI/DTOs/CreateOrganizationDTO.cs
@@ -1,9 +1,22 @@
-﻿namespace LavenderSpiritAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LavenderSpiritAPI.DTOs
 {
     public class CreateOrganizationDTO
     {
-        public string Name { get; set; }
         public string Email { get; set; }
+        [Required(ErrorMessage = "Adres email jest wymagany.")]
+        [EmailAddress(ErrorMessage = "Nieprawidłowy format adresu email.")]
+
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Nazwa organizacji jest wymagana.")]
+        [StringLength(100, ErrorMessage = "Nazwa organizacji nie może przekraczać 100 znaków.")]
+
         public string Password { get; set; }
+        [Required(ErrorMessage = "Hasło jest wymagane.")]
+        [RegularExpression(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$",
+            ErrorMessage = "Hasło musi mieć co najmniej 8 znaków i zawierać: cyfrę, małą i wielką literę oraz znak specjalny.")]
+    
     }
+
 }
